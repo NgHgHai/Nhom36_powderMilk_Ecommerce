7c95d246f4d7fed8d64976ca2e2ba08a434a8781
Thanh_Ngoan <20130335@st.hcmuaf.edu.vn>
2022-12-15 09:38:27 +0700

    add JDBI connection

---
+++ src/main/java/com/nhom36/milkPowder/model/Category.java
@@ -0,0 +1,46 @@
+package com.nhom36.milkPowder.model;
+
+import java.time.LocalDateTime;
+import java.util.Objects;
+
+public class Category {
+
+    private String id;
+    private String name;
+    private String desc;
+    private LocalDateTime createAt;
+    private LocalDateTime updateAt;
+    public Category(){}
+
+    public Category(String id, String name, String desc, LocalDateTime createAt, LocalDateTime updateAt) {
+        this.id = id;
+        this.name = name;
+        this.desc = desc;
+        this.createAt = createAt;
+        this.updateAt = updateAt;
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Category category = (Category) o;
+        return Objects.equals(id, category.id) && Objects.equals(name, category.name) && Objects.equals(desc, category.desc) && Objects.equals(createAt, category.createAt) && Objects.equals(updateAt, category.updateAt);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id, name, desc, createAt, updateAt);
+    }
+
+    @Override
+    public String toString() {
+        return "Category{" +
+                "id='" + id + '\'' +
+                ", name='" + name + '\'' +
+                ", desc='" + desc + '\'' +
+                ", createAt=" + createAt +
+                ", updateAt=" + updateAt +
+                '}';
+    }
+}
