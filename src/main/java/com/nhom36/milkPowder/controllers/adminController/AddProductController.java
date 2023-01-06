2883ccb5fde315735f80781ced9db94a5a7cfe73
Hoang Hai Laptop <20130166@st.hcmuaf.edu.vn>
2023-01-06 13:57:13 +0700

    upload file

--- src/main/java/com/nhom36/milkPowder/controllers/adminController/AddProductController.java
+++ src/main/java/com/nhom36/milkPowder/controllers/adminController/AddProductController.java
@@ -1,19 +1,69 @@
 package com.nhom36.milkPowder.controllers.adminController;
 
+import com.nhom36.milkPowder.beans.Product;
+import com.nhom36.milkPowder.services.ProductService;
+import com.nhom36.milkPowder.util.StringUtil;
+import org.apache.commons.beanutils.BeanUtils;
+
 import javax.servlet.*;
 import javax.servlet.http.*;
 import javax.servlet.annotation.*;
 import java.io.IOException;
+import java.lang.reflect.InvocationTargetException;
 
 @WebServlet(name = "AddProductController", value = "/AddProductController")
+@MultipartConfig(
+        fileSizeThreshold = 1024 * 1024 * 1, // 1 MB
+        maxFileSize = 1024 * 1024 * 10,      // 10 MB
+        maxRequestSize = 1024 * 1024 * 100   // 100 MB
+)
 public class AddProductController extends HttpServlet {
+    ProductService productService = new ProductService();
+
     @Override
     protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-    doPost(request, response);
+
+        String id = request.getParameter("id");
+        Product product = productService.getById(id);
+        request.setAttribute("product", product);
+        request.getRequestDispatcher("admin/addProduct.jsp").forward(request, response);
     }
 
     @Override
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-        request.getRequestDispatcher("admin/addProduct.jsp").forward(request,response);
+        Product product = new Product();
+
+        handleUploadFile(request, response);
+        try {
+            BeanUtils.populate(product, request.getParameterMap());
+            System.out.println(product);
+            if (product.getId() == null || product.getId().equals("")) {
+                String id = StringUtil.getIDWithLength(10);
+                product.setId(id);
+                productService.insert(product);
+                response.sendRedirect("/AddProductController?id=" + id);
+                System.out.println("insertt");
+            } else {
+                productService.updateProduct(product);
+                request.setAttribute("mess", "Upload thanh cong");
+                doGet(request, response);
+                System.out.println("update");
+            }
+        } catch (IllegalAccessException e) {
+            throw new RuntimeException(e);
+        } catch (InvocationTargetException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    void handleUploadFile(HttpServletRequest request, HttpServletResponse response) {
+        try {
+            Part part = request.getPart("imgDisplay");
+            part.write("E:/" + part.getSubmittedFileName());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        } catch (ServletException e) {
+            throw new RuntimeException(e);
+        }
     }
 }
